ff90ef17cb50b4fa39b079efcde4b1bb3dd1ae28
Janne Nykänen <janne.j.nykanen@outlook.com>
2022-08-09 16:27:44 +0300

    Lisätty hakupalkki ja GetProductByName hakumetodi

--- ShopDb/Controllers/UserController.cs
+++ ShopDb/Controllers/UserController.cs
@@ -183,6 +183,19 @@ namespace ShopDb.Controllers
 
         public IActionResult Login(int Id)
         {
+            var id = (from i in _context.Users
+                     where i.Id == Id
+                     select i.Id).FirstOrDefault();
+            if (id == 0)
+            {
+                ViewBag.LoginFailedMessage = "Invalid user Id, please rety or create a new user";
+            }
+            else
+            {
+                ViewBag.LoginFailedMessage = "";
+            }
+            
+          
             HttpContext.Session.SetInt32("userId", Id);
             return View();
         }
