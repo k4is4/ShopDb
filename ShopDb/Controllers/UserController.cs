85423406183c70cd654a8467bfdde7cddf42e694
Kaisa <kaisa.ranta@awacademy.training>
2022-08-11 15:23:55 +0300

    AdminId haetaan konfiguroinnista, UserControlleria siivottu

--- ShopDb/Controllers/UserController.cs
+++ ShopDb/Controllers/UserController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShopDb.Models;
 
@@ -20,38 +15,6 @@ namespace ShopDb.Controllers
             var optionsBuilder = new DbContextOptionsBuilder<ShopDbContext>();
             optionsBuilder.UseSqlServer(_conf["ConnectionStrings:ShopDb"]);
             _context = new ShopDbContext(optionsBuilder.Options);
-        }
-
-        // GET: Users
-        public async Task<IActionResult> Index()
-        {
-            return _context.Users != null ?
-                        View(await _context.Users.ToListAsync()) :
-                        Problem("Entity set 'ShopDbContext.Users'  is null.");
-        }
-
-        // GET: Users/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Users == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
         }
 
         [HttpGet]
@@ -91,95 +54,6 @@ namespace ShopDb.Controllers
             return View();
         }
 
-
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Users == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Phone,Email")] User user)
-        {
-            if (id != user.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Users == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'ShopDbContext.Users'  is null.");
-            }
-            var user = await _context.Users.FindAsync(id);
-            if (user != null)
-            {
-                _context.Users.Remove(user);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         private bool UserExists(int id)
         {
             return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
@@ -199,7 +73,7 @@ namespace ShopDb.Controllers
                 {
                     var obj = _context.Users.Where(a => a.Id.Equals(user.Id)).FirstOrDefault();
 
-                    if (obj.Id == 2)
+                    if (obj.Id == int.Parse(_conf["AdminId"]))
                     { return Redirect("/Admin/Product"); }
 
                     if (obj != null)
@@ -210,7 +84,7 @@ namespace ShopDb.Controllers
                     }
                 }
             }
-            ViewBag.LoginFailedMessage = "Invalid user Id, please rety or create a new user";
+            ViewBag.LoginFailedMessage = "Käyttäjä ID:tä ei löydy. Ole hyvä ja yritä uudelleen tai luo uusi käyttäjätili";
             return View(user);
         }
 
