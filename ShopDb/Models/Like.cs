13084babafdb6e8add92eb4eb7daeb4fa4a6afa0
Sizong <106967252+Sizong@users.noreply.github.com>
2022-08-12 00:23:04 +0300

    Lisätty "Tykkäys" luokka & referenssi tykkäystauluun

---
+++ ShopDb/Models/Like.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopDb.Models
+{
+    public partial class Like
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
